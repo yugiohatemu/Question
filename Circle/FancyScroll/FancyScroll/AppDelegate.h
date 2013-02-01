420d0c261432caaadca4b3820c75c9650dfb2c05
Yue <wei@v1424-wn-172-21-96-136.campus-dynamic.uwaterloo.ca>
2013-01-31 19:29:48 -0500

    Initialize for difficult challenge

---
+++ Circle/FancyScroll/FancyScroll/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FancyScroll
+//
+//  Created by Yue on 1/31/13.
+//  Copyright (c) 2013 Yue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
